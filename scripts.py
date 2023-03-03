82a07a33d679f072438008ab20fc46c4cb4937a7
Andres Estepa <andres.estepa1@correo.uis.edu.co>
2023-03-03 18:57:30 -0400

    First version with code to import the database

---
+++ scripts.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import mysql.connector
+
+database = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="M35DF678",
+    database="sakila"
+)
+
+db_tables = pd.read_sql("SHOW TABLES", database)
+
+
+print(db_tables)
